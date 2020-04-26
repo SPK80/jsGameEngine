6079eb5e9c450727ed1bad93ade74a346a33ccc3
Pavel Sukonkin <psukonkin@gmail.com>
2020-04-26 08:42:34 +0200

    walkMan

--- gameObjects/walkMan.js
+++ gameObjects/walkMan.js
@@ -1,15 +1,28 @@
+import { CircleCounter } from "../counters";
+
 export class walkMan extends GameObject {
-	#phase = 0;
-	constructor(x, y, ) {
+	#phase = new CircleCounter(0, 3, 1);
+	#image = new Image();
+	constructor(x, y) {
 		super({
 			x: x == undefined ? 100 : x,
 			y: y == undefined ? 100 : y,
 		});
+
+		this.#image.src = 'sprite.png';
 	}
-	
+
+
+
 	draw(render) {
-		this.#phase ++;
-		if ()
+		const p = this.#phase.getNext();
+
+		render.sprite({
+			image: this.#image,
+			width: 100,
+			height: 100,
+			currentFrame = p
+		})
 	}
 
 }
