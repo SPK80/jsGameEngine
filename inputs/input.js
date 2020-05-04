4e0d0d11e4408d9eb779b8b0941134919d6f959e
SPK80 <psukonkin@gmail.com>
2020-05-04 07:43:06 +0300

    refac & fix keybInput

--- inputs/input.js
+++ inputs/input.js
@@ -6,7 +6,7 @@ export class Input {
 
 export class KeyBoardInput extends Input {
 
-	#pressedKeys = new PressedKeys();
+	#actions = [];
 	#keyMap = null;
 
 	constructor(keyMap) {
@@ -15,62 +15,82 @@ export class KeyBoardInput extends Input {
 			this.#keyMap = keyMap;
 
 		window.addEventListener('keydown', e => {
-			this.#pressedKeys.add(e.keyCode);
+			const act = this.#keyMap.get(e.keyCode);
+			if (act && !this.#actions.includes(act))
+				this.#actions.push(act);
+			console.log('keydown ', e.keyCode, this.#actions);
 		});
 
 		window.addEventListener('keyup', e => {
-			// console.log(e.keyCode);
-			// console.log(this.#pressedKeys);
-
-			this.#pressedKeys.remove(e.keyCode);
-			// console.log(this.#pressedKeys);
+			const actions = [];
+			for (let i = 0; i < this.#actions.length; i++) {
+				const action = this.#actions[i];
+				if (action != this.#keyMap.get(e.keyCode))
+					actions.push(action);
+			}
+			this.#actions = actions;
+			console.log('keyup ', e.keyCode, this.#actions);
 		});
 	}
 
 	get() {
-		const actions = [];
-		this.#pressedKeys.items.forEach(key => {
-			const act = this.#keyMap.get(key);
-			if (act)
-				actions.push(act);
-		});
-		return actions;
-	}
-
-	// keyPressed(key) {
-	// 	if (typeof (key) == 'number')
-	// 		return this.#pressedKeys[key];
-	// 	else if (typeof (key) == 'string')
-	// 		return this.#pressedKeys[this.#keys[key]];
-	// 	else return undefined;
-	// }
-}
-
-export class PressedKeys {
-	#items = [];
-	add(key) {
-		if (!this.#items.includes(key))
-			this.#items.push(key);
-	}
-
-	remove(key) {
-		const i = this.#items.indexOf(key);
-		// console.log(i, key, this.#items);
-
-		delete this.#items[i];
-		const items = [];
-		this.#items.forEach(it => {
-			this.#items.push(it);
-		});
-		// console.log(items);
-		this.#items = items;
-	}
-
-	get(key) {
-		return this.#items.includes(key);
-	}
-
-	get items() {
-		return this.#items;
+		return this.#actions;
 	}
 }
+
+
+// export class AltKeyBoardInput extends Input {
+
+// 	#pressedKeys = new PressedKeys();
+// 	#keyMap = null;
+
+// 	constructor(keyMap) {
+// 		super();
+// 		if (keyMap instanceof KeyMap)
+// 			this.#keyMap = keyMap;
+
+// 		window.addEventListener('keydown', e => {
+// 			this.#pressedKeys.add(e.keyCode);
+// 		});
+
+// 		window.addEventListener('keyup', e => {
+// 			this.#pressedKeys.remove(e.keyCode);
+// 		});
+// 	}
+
+// 	get() {
+// 		const actions = [];
+// 		this.#pressedKeys.items.forEach(key => {
+// 			const act = this.#keyMap.get(key);
+// 			if (act)
+// 				actions.push(act);
+// 		});
+// 		return actions;
+// 	}
+// }
+
+
+// export class PressedKeys {
+// 	#items = [];
+// 	add(key) {
+// 		if (!this.#items.includes(key))
+// 			this.#items.push(key);
+// 	}
+
+// 	remove(key) {
+// 		const items = [];
+// 		for (let i = 0; i < this.#items.length; i++) {
+// 			const item = this.#items[i];
+// 			if (item != key) items.push(item);
+// 		}
+// 		this.#items = items;
+// 	}
+
+// 	get(key) {
+// 		return this.#items.includes(key);
+// 	}
+
+// 	get items() {
+// 		return this.#items;
+// 	}
+// }
