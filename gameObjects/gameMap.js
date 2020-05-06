d4a902efe9da9a2ff62ffcb835f969c1f1db5e07
SPK80 <psukonkin@gmail.com>
2020-05-06 16:56:32 +0300

    gameMap

--- gameObjects/gameMap.js
+++ gameObjects/gameMap.js
@@ -1,36 +1,54 @@
 import { BaseObject } from "./gameObject.js";
 
-export class GameMap extends BaseObject {
+export class Landscape extends BaseObject {
 
 	#tiles = new Image();
 	#imageLoaded = false;
 	#tileWidth = 0;
 	#tileHeight = 0;
+	#tilesInRow = 0;
 
 	#xCount = 0;
 	#yCount = 0;
 
-	#cells = [[]];
+	#cells;
 
-	constructor(name, tiles) {
+	constructor(name, tiles, tileWidth, tileHeight, cells) {
 		super({ name: name });
 		this.#tiles = tiles;
+		this.#cells = cells;
+		this.#tileWidth = tileWidth;
+		this.#tileHeight = tileHeight;
+		this.#tilesInRow = this.#tiles.width / this.#tileWidth;
+
+	}
+
+	tilePos(cell) {
+		return {
+			x: cell % this.#tilesInRow,
+			y: Math.floor(cell / this.#tilesInRow)
+		}
 	}
 
 	update(drivers) {
-		const r = drivers.render;
+		const rndr = drivers.render;
 		for (let r = 0; r < this.#cells.length; r++) {
 			const row = this.#cells[r];
 			for (let c = 0; c < row.length; c++) {
 				const cell = row[c];
-				r.tile({
+				const pos = this.tilePos(cell);
+				rndr.tile({
 					image: this.#tiles,
-					x:,
-					y:,
+					tileX: pos.x,
+					tileY: pos.y,
+					x: c * this.#tileWidth,
+					y: r * this.#tileHeight,
 					width: this.#tileWidth,
 					height: this.#tileHeight,
+					tileWidth: this.#tileWidth,
+					tileHeight: this.#tileHeight,
 				});
 			}
-
 		}
 	}
+}
--- scenes/pacScene.js
+++ scenes/pacScene.js
@@ -2,13 +2,19 @@ import { Scene } from "./scene.js";
 import { Pacman } from "../gameObjects/pacman.js";
 import { WalkMan } from "../gameObjects/walkMan.js";
 import { Pers } from "../gameObjects/pers.js";
-import { GameMap } from "../gameObjects/gameMap.js";
+import { Landscape } from "../gameObjects/gameMap.js";
 
 export class PacScene extends Scene {
 
 	constructor(name, drivers, tiles) {
-
-		const map = new GameMap('GameMap', tiles);
+		const lendCells = [30, 31, 46, 47];
+		const cells = new Array()
+		const landscape = new Landscape('GameMap', tiles, 32, 32, [
+			[31, 30, 30, 30],
+			[30, 31, 31, 30],
+			[30, 30, 31, 30],
+			[30, 30, 30, 30],
+		]);
 
 		const walcMan = new Pers('Pers', 10, 100, tiles);
 		const pac = new Pacman('Pacman', 100, 100, '#00F0FF', 50);
@@ -22,7 +28,7 @@ export class PacScene extends Scene {
 		super({
 			name: name,
 			drivers: drivers,
-		}, [pac, walcMan]
+		}, [landscape, walcMan]
 		);
 	}
 }
