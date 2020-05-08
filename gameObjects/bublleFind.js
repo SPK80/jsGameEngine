fd09c69ec8f1dacea64f54122124f667da690224
SPK80 <psukonkin@gmail.com>
2020-05-08 08:15:21 +0300

    refac & fix bublleFind

--- gameObjects/bublleFind.js
+++ gameObjects/bublleFind.js
@@ -1,41 +1,61 @@
-// export function bublleFind(array, condition) {
-// 	if (condition(array, 0, array.length - 1))
-// 		return find(array, 0, array.length - 1, condition);
+import { throwIfUndefined } from "../classUtils.js";
+
+/// callback  - function(index) returns value by index for comparison  
+export function bublleFindIndex(findVal, length, callback) {
+	const _MAXDEEP = 10000;
+	let _deep = 0;
+	throwIfUndefined(callback);
+	throwIfUndefined(findVal);
+	throwIfUndefined(length);
+
+	const find = (b, e) => {
+		_deep++;
+		if (_deep > _MAXDEEP) throw (`${_deep} its too deep!`);
+
+		if (e - b < 2) return e;
+		const c = Math.floor(e + b) / 2;
+		if (findVal >= callback(b) && findVal <= callback(c))
+			return find(b, c);
+		else
+			return find(c, e);
+	};
+
+	if (findVal >= callback(length - 1)) return length;
+	if (findVal <= callback(0)) return 0;
+
+	return find(0, length - 1);
+}
+
+// function find(b, e) {
+// 	// console.log(b, e);
+// 	_deep++;
+// 	if (e - b < 2) return e;
+// 	const c = Math.floor(e + b) / 2;
+// 	if (_deep > _MAXDEEP) throw (_deep);
+// 	if (_findVal >= _callback(b) && _findVal <= _callback(c))
+// 		return find(b, c);
+// 	else
+// 		return find(c, e);
 // }
-const maxDeep = 20;
-let deep = 0;
-// function find(array, b, e, condition) {
+
+// export function bublleFind(array, val) {
+// 	const last = array.length - 1;
+// 	if (val >= array[last]) return last + 1;
+// 	if (val <= array[0]) return 0;
+// 	return find(array, 0, last, val);
+// }
+
+// function find(array, b, e, val) {
+// 	console.log(b, e);
 // 	deep++;
-// 	if (e == b) return e;
-// 	const c = Math.floor(e - b) / 2;
-// 	if (deep > maxDeep) throw (b, e, c);
+// 	if (e - b < 2) return e;
 
-// 	console.log(b, e, c);
-// 	if (condition(array, b, c)) {
-// 		return find(array, b, c, condition);
-// 	} else {
-// 		return find(array, c, e, condition);
-// 	}
+// 	const c = Math.floor(e + b) / 2;
+
+// 	if (deep > maxDeep) throw (deep);
+
+// 	if (val >= array[b] && val <= array[c])
+// 		return find(array, b, c, val);
+// 	else
+// 		return find(array, c, e, val);
 // }
-
-export function bublleFind(array, val) {
-	const last = array.length - 1;
-	if (val >= array[last]) return last;
-	if (val <= array[0]) return 0;
-	return find(array, 0, last, val);
-}
-
-function find(array, b, e, val) {
-	deep++;
-	if (e - b < 2) return b;
-	const c = Math.floor(e + b) / 2;
-	console.log(b, e, c);
-
-	if (deep > maxDeep) throw (deep);
-
-	if (val >= array[b] && val <= array[c])
-		return find(array, b, c, val);
-	else
-		return find(array, c, e, val);
-
-}
--- testBublle.js
+++ testBublle.js
@@ -1,10 +1,10 @@
-import { bublleFind } from "./gameObjects/bublleFind.js";
-const arr = [1, 5, 6, 10, 11];
-const n = 0;
+import { bublleFindIndex } from "./gameObjects/bublleFind.js";
+const arr = [1, 5, 5, 10, 11];
+const n = 4;
 
-const cond = function (arr, b, e) {
-	return n >= arr[b] && n <= arr[e];
-}
 console.log(arr, n);
-const r = bublleFind(arr, n);
-console.log(r);
+const r = bublleFindIndex(n, arr.length, (i) => arr[i]);
+
+console.log(`[${r}]:${arr[r]}`);
+arr.splice(r, 0, n);
+console.log(arr);
